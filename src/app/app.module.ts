91a7384392ce626ecf65e5c150fb03e14e147aae
Tejaswikatreddy <42598383+Tejaswikatreddy@users.noreply.github.com>
2018-10-19 19:25:40 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,68 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule, MatToolbarModule, MatListModule } from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { SignupComponent } from './components/signup/signup.component';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import { httpService } from './services/http.service';
+import { FormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
-    AppComponent,
+      AppComponent,
     LoginComponent,
-    DashboardComponent
+    SignupComponent,
+    ForgotPasswordComponent,
+    DashboardComponent,
+    ResetPasswordComponent,
+    SidebarComponent,
+    ToolbarComponent,
+    NavbarComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    MatFormFieldModule,
+    MatInputModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatIconModule,
+    FormsModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    MatSidenavModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatListModule,
+    MatMenuModule
   ],
-  providers: [],
+  providers: [
+    httpService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
